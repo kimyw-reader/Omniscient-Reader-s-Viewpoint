80b28fc98b3181446773081d151bd726b29688f6
kimyw-reader <gimu593@gmail.com>
2024-11-02 17:52:24 +0900

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,21 @@
+import random 
+a = (random.randrange())
+if a==0:
+        a=a+1
+    
+a = random.randrange(6)+1
+imprt.random
+print('-'*10"동전 던지기"'-'*10)
+coin = random.randrange
+if coin == 0 :
+    print("앞면")
+else :
+    print("뒷면")
+
+import turtle
+t1 = turtle.Turtle()
+screen = turtle.screan()
+image = "500won_front.gif"
+screan.addshape(image)
+t1.shape(image)
+t1.stamp()
